66451488348c1659a1a5d3a57f7f66545c7a424b
Hongyu Dang <77089121+danghongyu@users.noreply.github.com>
2023-09-14 22:11:42 -0500

    Update CheckM.sh

--- S04_bin_quality_evaluation_and_refining/CheckM.sh
+++ S04_bin_quality_evaluation_and_refining/CheckM.sh
@@ -1,1 +1,5 @@
-
+for file in SRR15035645 SRR15035646 SRR15035647 SRR15035648; do
+        (cd "$file";
+         checkm lineage_wf -t 20 -x fa anvio_output/final.contigs_fixed.fa.metabat* anvio_output/checkM_output
+        )
+done
