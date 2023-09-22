11dcef5663382eb60893eac276028d8186d72c39
Hongyu Dang <77089121+danghongyu@users.noreply.github.com>
2023-09-21 22:10:36 -0400

    Update prokka.sh

--- S05_MAG_dereplication_taxonomy_and_function/prokka.sh
+++ S05_MAG_dereplication_taxonomy_and_function/prokka.sh
@@ -1,1 +1,4 @@
-
+for f in bins_combine_derep/*.fa; 
+do
+    prokka --cpus 20 --outdir bins_combine_derep/prokka_output/"$(basename "$f")"_genome --prefix "$(basename "$f")" bins_combine_derep/"$(basename "$f")"; 
+done
