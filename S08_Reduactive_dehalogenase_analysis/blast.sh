cbed1dfa7e4626c9c2a9f4c613094d5883577ef1
Hongyu Dang <77089121+danghongyu@users.noreply.github.com>
2023-09-22 10:50:20 -0400

    Update blast.sh

--- S08_Reduactive_dehalogenase_analysis/blast.sh
+++ S08_Reduactive_dehalogenase_analysis/blast.sh
@@ -1,1 +1,5 @@
+export BLASTDB_LMDB_MAP_SIZE=100000000
 
+makeblastdb -in all_site_RDase.fasta -dbtype nucl -out Rase_PCB_site
+
+blastn -query all_site_RDase.fasta -db Rase_PCB_site -out alignment.txt
