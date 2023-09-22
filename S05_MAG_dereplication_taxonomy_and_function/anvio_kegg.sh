99cb7366893afad7931f4ca70ed5a87db8303332
Hongyu Dang <77089121+danghongyu@users.noreply.github.com>
2023-09-21 22:09:01 -0400

    Update anvio_kegg.sh

--- S05_MAG_dereplication_taxonomy_and_function/anvio_kegg.sh
+++ S05_MAG_dereplication_taxonomy_and_function/anvio_kegg.sh
@@ -1,1 +1,15 @@
+for file in *.fa
+do
+    ID=${file%.fa}
+    anvi-gen-contigs-database -f $file -o anvio_bins_db/"${ID}.db" -n $ID -T 20
+done
 
+for file in anvio_bins_db/*.db
+do
+    anvi-run-kegg-kofams -c $file -T 20
+done
+
+anvi-estimate-metabolism -e anvio_bins_db/KEGG_completeness_1/external-genomes.txt \
+                         -O anvio_bins_db/KEGG_completeness_1/All_bins \
+                         --module-completion-threshold 1 \
+                         --matrix-format
